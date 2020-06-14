00cb307c89984a2daa6b22cadf4c781cb8581402
Daniel A Koontz <dak.7997@gmail.com>
2020-06-14 20:02:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function theBeatlesPlay(musicians, instruments) {
   var theyPlay = []
 
   for (let i = 0; i < musicians.length; i++) {
-
+    theyPlay.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 }
